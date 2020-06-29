515e08001cacba122f259f6713f66a21d7d2318a
kazuma.suzuki <kazuma.001600@gmail.com>
2020-06-29 14:54:15 +0000

    user login

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,12 +1,16 @@
 @extends("layouts.app")
 
 @section("content")
-<div class="center jumbotron">
-    <div class="text-center">
-        <h1>Welcome to the Microposts</h1>
+    @if(Auth::check())
+        {{ Auth::user()->name }}
+    @else
+        <div class="center jumbotron">
+            <div class="text-center">
+                <h1>Welcome to the Microposts</h1>
         
-        {!! link_to_route("signup.get", "Sign up now!",[],["class" => "btn btn-lg btn-primary"]) !!}
+                {!! link_to_route("signup.get", "Sign up now!",[],["class" => "btn btn-lg btn-primary"]) !!}
         
-    </div>
-</div>
+            </div>
+        </div>
+    @endif
 @endsection

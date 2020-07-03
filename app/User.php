956add4033c1b1d26d9209c7c8bf531a6ecb2ee4
kazuma.suzuki <kazuma.001600@gmail.com>
2020-07-03 09:21:20 +0000

    follow,unfollow

--- app/User.php
+++ app/User.php
@@ -44,18 +44,48 @@ class User extends Authenticatable
     
     public function loadRelationshipCounts()
     {
-        $this->loadCount("microposts");
+        $this->loadCount(["microposts","followings","followers"]);
     }
     
-    public function show($id)
+    public function followings()
     {
-        $user =User::findOrFail($id);
-        $user->loadRelationshipCounts();
-        $microposts =$user->microposts()->orderBy("created_at","desc")->paginate(10);
-        return view("users.show",[
-            "user" => $user,
-            "microposts" =>$microposts,
-            ]);
+        return $this->belongsToMany(User::class,"user_follow","user_id","follow_id")->withTimestamps();
+    }
+    
+    public function followers()
+    {
+        return $this->belongsToMany(User::class,"user_follow","follow_id","user_id")->withTimestamps();
+    }
+    
+    public function follow($userId)
+    {
+        $exist = $this->is_following($userId);
         
+        $its_me = $this->id == $userId;
+        
+        if($exist || $its_me){
+            return false;
+        }else{
+            $this->followings()->attach($userId);
+            return true;
+        }
+    }
+    
+    public function unfollow($userId)
+    {
+        $exist = $this->is_following($userId);
+        $its_me =$this->id == $userId;
+        
+        if($exist && !$its_me){
+            $this->followings()->detach($userId);
+            return true;
+        }else{
+            return false;
+        }
+    }
+    
+    public function is_following($userId)
+    {
+        return $this->followings()->where("follow_id",$userId)->exists();
     }
 }
